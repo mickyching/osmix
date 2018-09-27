0b1fce4ede95ee5e5b35d13776fa3f5a33b5e6b2
Micky Ching <mickyching@qq.com>
2018-09-27 10:10:49 -0400

    bin: add mm.go-get-packages
    Signed-off-by: Micky Ching <mickyching@qq.com>

---
+++ bin/mm.go-get-packages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo go get github.com/rogpeppe/godef
+go get github.com/rogpeppe/godef
+echo go get github.com/nsf/gocode
+go get github.com/nsf/gocode
+
+mkdir -p $GOPATH/src/golang.org/x
+cd $GOPATH/src/golang.org/x
+
+echo git clone https://github.com/golang/net
+git clone https://github.com/golang/net
+echo go install golang.org/x/net
+go install golang.org/x/net
+
+echo git clone https://github.com/golang/tools
+git clone https://github.com/golang/tools
+echo go install golang.org/x/tools/cmd/...
+go install golang.org/x/tools/cmd/...
